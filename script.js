5bd2601e58a30ea74a629a50191bd469d7c2c915
chetan1bro <87486003+chetan1bro@users.noreply.github.com>
2022-01-31 05:40:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function startVideo() {
   )
 }
 
-video.addEventListener('playing', () => {
+video.addEventListener('playi', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
